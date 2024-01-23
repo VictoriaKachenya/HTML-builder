3710d25c08a09856365d10fdb66fa776f1d95a4b
Victoria <victoriakachenya@gmail.com>
2024-01-23 15:27:31 +0300

    feat: add the third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, (error, files) => {
+    files.forEach((file) => {
+        fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+            if (stats.isFile()) {
+                console.log(`${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${(stats.size / 1000).toFixed(3)}kb`)
+            }
+        })
+    }
+    )
+});
