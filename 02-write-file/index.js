2adf22d07476b12ae82bebeef9545f9b24cf359a
Victoria <victoriakachenya@gmail.com>
2024-01-23 15:22:25 +0300

    feat: add the second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+stdout.write('Hello!\nEnter your text\n');
+fs.appendFile(path.join(__dirname, '02-write-file.txt'), '', (err) => {
+});
+stdin.on('data', (data) => {
+    if (data.toString().includes('exit')) process.exit();
+    fs.appendFile(path.join(__dirname, '02-write-file.txt'), data.toString(), (err) => {
+    })
+});
+
+process.on('exit', () => {
+    console.log('See you soon!')
+});
+
+process.on('SIGINT', () => {
+    process.exit();
+});
